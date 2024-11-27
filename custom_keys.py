2a9d9a9719b327cf84b5aa42d7cb4b09cb1cc84b
Oleg Jarma Montoya <ojarmam@unal.edu.co>
2024-11-27 14:47:02 +0100

    add pygame controller test

--- custom_keys.py
+++ custom_keys.py
@@ -13,8 +13,6 @@ def get_custom_keys_to_action(action_set, joystick=False):
         Dictionary of key values to actions
     """
     if joystick:
-        pygame.init()
-        pygame.joystick.init()
         controller = pygame.joystick.Joystick(0)
 
         UP = pygame.CONTROLLER_BUTTON_DPAD_UP
