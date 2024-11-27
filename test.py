c10531589d863c75288f2ecdad2a34fe7740a00d
Oleg Jarma Montoya <ojarmam@unal.edu.co>
2024-11-27 14:32:42 +0100

    add pygame controller test

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
 from game import GameScreen
+import pygame
 import uuid
 
 
 
 def main():
-    GameScreen(
-        participant_id=0,
-        game_name = "ALE/Centipede-v5",
-        time_limit=15
-    )
+    # GameScreen(
+    #     participant_id=0,
+    #     game_name = "ALE/Centipede-v5",
+    #     time_limit=15
+    # )
+    pygame.joystick.init()
+    controller = pygame.joystick.Joystick(0)
+    while True:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.JOYBUTTONDOWN:
+                print("Button Pressed")
+            #     if j.get_button(6):
+            #         # Control Left Motor using L2
+            #     elif j.get_button(7):
+            #         # Control Right Motor using R2
+            # elif event.type == pygame.JOYBUTTONUP:
+            #     print("Button Released")
 
 
 if __name__ == "__main__":
