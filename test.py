510d8fca1cfcfd1b7c502738fc18c44b4532d032
unknown <olegbakugan@gmail.com>
2025-03-06 14:07:31 +0100

    add gazepoint integration

--- test.py
+++ test.py
@@ -3,41 +3,46 @@ from random import shuffle
 import numpy as np
 import uuid
 import itertools
+import subprocess
+import sys
 
 from brainlablsl import create_stream
+from brainlabgp3 import BrAInLabGP3
 
 def main():
     participant_id = str(uuid.uuid4())
     with open("psychoatari.yml", 'r') as f:
         stream = create_stream(f)
 
-    game_details = {
-        "Turmoil": {"modes": [x for x in range(4)], "difficulties": [0]},
-        "WordZapper": {
-            "modes": [x for x in range(4)],
-            "difficulties": [x for x in range(4)],
-        },
-        "Boxing": {"modes": [0], "difficulties": [x for x in range(4)]},
-    }
+#     game_details = {
+#         "Turmoil": {"modes": [x for x in range(4)], "difficulties": [0]},
+#         "WordZapper": {
+#             "modes": [x for x in range(4)],
+#             "difficulties": [x for x in range(4)],
+#         },
+#         "Boxing": {"modes": [0], "difficulties": [x for x in range(4)]},
+#     }
 
-    game_list = {
-        k: list(itertools.product(*v.values())) for (k, v) in game_details.items()
-    }
-    game_names = list(game_details.keys())
+#     game_list = {
+#         k: list(itertools.product(*v.values())) for (k, v) in game_details.items()
+#     }
+#     game_names = list(game_details.keys())
 
-    shuffle(game_names)
+#     shuffle(game_names)
+
+    subprocess.Popen([sys.executable, 'record.py'])
 
     StartScreen(countdown=5).run()
 
-    GameScreen(
-        participant_id="participant_id",
-        game_name=f"{game_names[0]}-v5",
-        time_limit=120,
-        tutorial=True,
-        trial_number=0,
-        logs_path="test_logs",
-        stream=stream
-    )
+#     GameScreen(
+#         participant_id="participant_id",
+#         game_name=f"{game_names[0]}-v5",
+#         time_limit=120,
+#         tutorial=True,
+#         trial_number=0,
+#         logs_path="test_logs",
+#         stream=stream
+#     )
 
 
 if __name__ == "__main__":
